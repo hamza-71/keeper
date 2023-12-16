556e6941f8cfa72bb335476297ad87df21df0586
hamza-71 <hamzalaabidi.dracoss@gmail.com>
2023-12-16 03:05:16 -0800

    second commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from "cors";
 import mongoose from "mongoose";
 import noteRouter from "./routes/note.js"
 import authRouter from "./routes/auth.js"
+import dotenv from "dotenv"
+dotenv.config();
+
+
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -20,10 +24,10 @@ app.options("*", cors());
 app.use("/",noteRouter)
 app.use("/auth",authRouter)
 /*-----------------databaseConnection-------------------------*/
-const PORT = 8000;
+const PORT = process.env.PORT;
 mongoose.set("strictQuery", false);
 mongoose
-  .connect("mongodb://127.0.0.1:27017/NoteList")
+  .connect(process.env.MONGOURL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running successfuly : ${PORT}`))
   )
